bdc5c1bb7ee9ef630080efd1863963cd27d6a157
Paolo <str237@gmail.com>
2020-03-16 02:57:06 +0100

    Using JWT to give admin role to auth service

--- auth/main.py
+++ auth/main.py
@@ -2,17 +2,30 @@ from fastapi import FastAPI
 import logging
 import httpx
 import os
+import jwt
+import uvicorn
 
 logger = logging.getLogger("main")
 
 app = FastAPI()
-client = httpx.AsyncClient()
-jwt_key = os.environ.get('JWT_KEY')
+KEY = os.environ.get('JWT_KEY')
+ALGORITHM = 'HS256'
+body = {
+    "https://hasura.io/jwt/claims": {
+        "x-hasura-default-role": "admin",
+        "x-hasura-allowed-roles": ["admin"]
+    }
+}
+ADMIN_JWT = jwt.encode(body, KEY, algorithm=ALGORITHM)
+default_headers = {
+    "Authorization": "Bearer "+ ADMIN_JWT.decode("utf-8")
+}
+
+client = httpx.AsyncClient(headers=default_headers)
 
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
-
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: str = None):
